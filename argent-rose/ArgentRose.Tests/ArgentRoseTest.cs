419ef14a51621b37379db5bbe15827bbf9a9bc90
Joser Gala Naranjo <jgalanar@gmail.com>
2026-06-12 12:23:25 +0200

    test: add a test case for quality 49 with sellIn higher than six

--- argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
+++ argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
@@ -50,14 +50,16 @@ public class ArgentRoseTest
 		Assert.That(products, Is.EquivalentTo(new List<Product> { updatedProduct }));
 	}
 
-	[Test]
-	public void SellIn_Higher_Than_Six_Updates_Quality_No_Higher_Than_Fifty()
+	[TestCase(10, 9, 49, 50)]
+	[TestCase(10, 9, 50, 50)]
+	public void SellIn_Higher_Than_Six_Updates_Quality_No_Higher_Than_Fifty(
+		int sellIn, int finalSellIn, int quality, int finalQuality)
 	{
-		var product = new Product(10, 50, "Theatre Passes");
+		var product = new Product(sellIn, quality, "Theatre Passes");
 
 		var products = InventoryUpdater.Execute([product]);
 
-		var updatedProduct = new Product(9, 50, "Theatre Passes");
+		var updatedProduct = new Product(finalSellIn, finalQuality, "Theatre Passes");
 		Assert.That(products, Is.EquivalentTo(new List<Product> { updatedProduct }));
 	}
 }

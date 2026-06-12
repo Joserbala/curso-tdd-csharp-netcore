5f77d1da9c2d8fc7a40e2bdf14ec862baa4df112
Joser Gala Naranjo <jgalanar@gmail.com>
2026-06-12 10:16:40 +0200

    feat: empty inventory returns empty inventory

--- argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
+++ argent-rose/ArgentRose.Tests/ArgentRoseTest.cs
@@ -1,14 +1,28 @@
-
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace ArgentRose.Tests
 {
-    public class ArgentRoseTest
-    {
-        [Test]
-        public void Think_A_Good_Name_And_Change_It()
-        {
-            Assert.That(false, Is.True);
-        }
-    }
+	public class ArgentRoseTest
+	{
+		[Test]
+		public void Empty_Inventory_Returns_Empty_Inventory()
+		{
+			var inventory = new Inventory();
+
+			var products = inventory.Update([]);
+
+			Assert.That(products.Count, Is.EqualTo(0));
+		}
+	}
+
+	internal class Product;
+
+	internal class Inventory
+	{
+		public List<Product> Update(List<Product> products)
+		{
+			return products;
+		}
+	}
 }

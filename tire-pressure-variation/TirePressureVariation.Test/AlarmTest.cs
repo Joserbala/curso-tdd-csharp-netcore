9addc7f7a4d3b6380079321030788b325cd4061e
Joser Gala Naranjo <jgalanar@gmail.com>
2026-06-15 12:57:52 +0200

    test: move to setup to remove duplication

--- tire-pressure-variation/TirePressureVariation.Test/AlarmTest.cs
+++ tire-pressure-variation/TirePressureVariation.Test/AlarmTest.cs
@@ -1,43 +1,42 @@
 using KataTirePressureVariation;
 using NSubstitute;
-using NSubstitute.ReceivedExtensions;
 using NUnit.Framework;
 
 namespace TirePressureVariation.Test
 {
     public class AlarmShould
     {
+        INotifier notifier;
+        IPressureSensor pressureSensor;
+        PressureMonitorization pressureMonitorization;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var safetyRange = new SafetyRange(17, 21);
+            notifier = Substitute.For<INotifier>();
+            pressureSensor = Substitute.For<IPressureSensor>();
+            pressureMonitorization = new PressureMonitorization(safetyRange, notifier, pressureSensor);
+        }
+
         [Test]
         public void Alarm_Deactivated_With_Dangerous_Pressure_Activates_The_Alarm()
         {
-            var safetyRange = new SafetyRange(17, 21);
-            var notifier = Substitute.For<INotifier>(); // spy
-            var pressureSensor = GetPressureSensorWith(16);
-            var pressureMonitorization = new PressureMonitorization(safetyRange, notifier, pressureSensor);
-
-            pressureMonitorization.Check();
+            pressureSensor.Get().Returns(16);
             
+            pressureMonitorization.Check();
+
             notifier.Received().Send("Alarm activated");
         }
 
         [Test]
         public void Alarm_Deactivated_With_Safe_Pressure_Remains_Deactivated()
         {
-            var safetyRange = new SafetyRange(17, 21);
-            var notifier = Substitute.For<INotifier>(); // spy
-            var pressureSensor = GetPressureSensorWith(17);
-            var pressureMonitorization = new PressureMonitorization(safetyRange, notifier, pressureSensor);
+            pressureSensor.Get().Returns(17);
 
             pressureMonitorization.Check();
 
-            notifier.DidNotReceiveWithAnyArgs().Send(Arg.Any<string>());
-        }
-
-        static IPressureSensor GetPressureSensorWith(int pressure)
-        {
-            var pressureSensor = Substitute.For<IPressureSensor>(); // stub
-            pressureSensor.Get().Returns(pressure);
-            return pressureSensor;
+            notifier.DidNotReceive().Send(Arg.Any<string>());
         }
     }
 }

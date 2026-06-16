c32b7a0c21efe6d4f0e0929539568005006e2ea4
Joser Gala Naranjo <jgalanar@gmail.com>
2026-06-16 12:02:19 +0200

    feat: customer orders coffee without sugar

--- coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
+++ coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
@@ -1,13 +1,35 @@
+using NSubstitute;
 using NUnit.Framework;
 
 namespace CoffeeMachine.Tests
 {
-    public class CoffeeMachineTest
-    {
-        [Test]
-        public void Fix_Me_And_Rename_Me()
-        {
-            Assert.That(false, Is.True);
-        }
-    }
+	public class CoffeeMachineTest
+	{
+		[Test]
+		public void Make_Coffee_Without_Sugar()
+		{
+			var drinkMaker = Substitute.For<IDrinkMaker>();
+			var coffeeMachine = new CoffeeMachine(drinkMaker);
+			coffeeMachine.SelectCoffee();
+
+			coffeeMachine.MakeDrink();
+
+			drinkMaker.Received(1).Send("C::");
+		}
+	}
+
+	public class CoffeeMachine(IDrinkMaker drinkMaker)
+	{
+		public void SelectCoffee() { }
+
+		public void MakeDrink()
+		{
+			drinkMaker.Send("C::");
+		}
+	}
+
+	public interface IDrinkMaker
+	{
+		void Send(string command);
+	}
 }

fb76f366619718c0e27c1009bd7a0b00427455c8
Joser Gala Naranjo <jgalanar@gmail.com>
2026-06-16 12:42:45 +0200

    test: remove duplication moving initialization to setup

--- coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
+++ coffee-machine/CoffeeMachine.Tests/CoffeeMachineTest.cs
@@ -5,13 +5,20 @@ namespace CoffeeMachine.Tests
 {
 	public class CoffeeMachineTest
 	{
+		IDrinkMaker drinkMaker;
+		CoffeeMachine coffeeMachine;
+
+		[SetUp]
+		public void Setup()
+		{
+			drinkMaker = Substitute.For<IDrinkMaker>();
+			coffeeMachine = new CoffeeMachine(drinkMaker);
+		}
+
 		[Test]
 		public void Make_Coffee_Without_Sugar()
 		{
-			var drinkMaker = Substitute.For<IDrinkMaker>();
-			var coffeeMachine = new CoffeeMachine(drinkMaker);
 			coffeeMachine.SelectCoffee();
-
 			coffeeMachine.MakeDrink();
 
 			drinkMaker.Received(1).Send("C::");
@@ -20,10 +27,7 @@ namespace CoffeeMachine.Tests
 		[Test]
 		public void Make_Tea_Without_Sugar()
 		{
-			var drinkMaker = Substitute.For<IDrinkMaker>();
-			var coffeeMachine = new CoffeeMachine(drinkMaker);
 			coffeeMachine.SelectTea();
-
 			coffeeMachine.MakeDrink();
 
 			drinkMaker.Received(1).Send("T::");
@@ -32,22 +36,17 @@ namespace CoffeeMachine.Tests
 		[Test]
 		public void Make_Chocolate_Without_Sugar()
 		{
-			var drinkMaker = Substitute.For<IDrinkMaker>();
-			var coffeeMachine = new CoffeeMachine(drinkMaker);
 			coffeeMachine.SelectChocolate();
-
 			coffeeMachine.MakeDrink();
 
 			drinkMaker.Received(1).Send("H::");
 		}
+
 		[Test]
 		public void Make_Coffee_With_One_Sugar_And_Stick()
 		{
-			var drinkMaker = Substitute.For<IDrinkMaker>();
-			var coffeeMachine = new CoffeeMachine(drinkMaker);
 			coffeeMachine.SelectCoffee();
 			coffeeMachine.AddOneSpoonOfSugar();
-
 			coffeeMachine.MakeDrink();
 
 			drinkMaker.Received(1).Send("C:1:0");
@@ -56,9 +55,6 @@ namespace CoffeeMachine.Tests
 		[Test]
 		public void Dont_Make_Drink()
 		{
-			var drinkMaker = Substitute.For<IDrinkMaker>();
-			var coffeeMachine = new CoffeeMachine(drinkMaker);
-
 			coffeeMachine.MakeDrink();
 
 			drinkMaker.DidNotReceiveWithAnyArgs().Send(Arg.Any<string>());
